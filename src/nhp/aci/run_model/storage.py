4a49102439755485145090ddeeb203f5fae5c858
Tom Jemmett <thomas.jemmett@nhs.net>
2026-05-07 10:37:42 +0100

    adds function for adding entry to table storage

--- src/nhp/aci/run_model/storage.py
+++ src/nhp/aci/run_model/storage.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from azure.core.credentials import TokenCredential
 from azure.core.exceptions import ResourceExistsError
+from azure.data.tables import TableClient
 from azure.storage.blob import BlobServiceClient
 
 from nhp.aci.config import Config
@@ -33,3 +34,29 @@ def upload_params_to_blob(
         logger.info("params uploaded to queue")
     except ResourceExistsError:
         logger.warning("file already exists, skipping upload")
+
+def add_table_storage_entry(
+    metadata: dict[str, Any],
+    model_run_id: str,
+    results_viewable: bool,
+    credential: TokenCredential,
+    config: Config,
+):
+    """Add model run to table storage.
+
+    Args:
+        metadata (dict[str, Any]): The metadata for the model run.
+        model_run_id (str): The model run ID.
+        results_viewable (bool): Whether the results are viewable.
+        credential (TokenCredential): Credential for authenticating with Azure.
+        config (Config): Configuration object.
+    """
+    table_client = TableClient(config.table_storage_endpoint, "modelruns", credential=credential)
+    entity = {
+        "PartitionKey": metadata["dataset"],
+        "RowKey": model_run_id,
+        "status": "submitted",
+        "viewable": results_viewable,
+        **metadata,
+    }
+    table_client.create_entity(entity)
